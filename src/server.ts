9527f403ae94cb2d006ee38b70ebe61bc5cc2dae
vinicius dos santos mota <vinicius_dos_santos_mota@hotmail.com>
2020-11-20 16:36:22 -0300

    Expose static files

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,11 @@ import Routes from './routes';
 import './database';
 import 'reflect-metadata';
 
+import uploadConfig from './config/upload';
+
 const app = express();
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(Routes);
 app.listen(3333, () => {
   console.log('Server start at port 3333');
