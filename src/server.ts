af4c37e0b465d8ed669aa6c486fa51cd90c2f024
vinicius dos santos mota <vinicius_dos_santos_mota@hotmail.com>
2020-12-30 09:32:37 -0300

    Add cors to api

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,20 @@
-import express, { NextFunction, Request, response, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import Routes from './routes';
 import './database';
 import 'reflect-metadata';
-
 import AppError from './errors/AppError';
 import uploadConfig from './config/upload';
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(Routes);
 app.use(
-  (error: Error, request: Request, resonse: Response, _next: NextFunction) => {
+  (error: Error, request: Request, response: Response, _next: NextFunction) => {
     if (error instanceof AppError) {
       return response
         .status(error.statusCode)
